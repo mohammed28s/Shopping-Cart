f23fd6203020b7092b17b266365901ae8a40205e
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2025-10-26 11:51:02 +0400

    This is Shopping Cart Project for simple eCommerce web app

---
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -0,0 +1,16 @@
+package com.dailycodework.dreamshops.model;
+
+
+import java.math.BigDecimal;
+import java.sql.DataTruncation;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+}
