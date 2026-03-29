8d72009823596afce947601b68c851d6e7754e09
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2026-03-29 13:36:01 +0400

    The main application has been added with a comment

--- src/main/java/com/dailycodework/dreamshops/DreamShopsApplication.java
+++ src/main/java/com/dailycodework/dreamshops/DreamShopsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DreamShopsApplication {
 
+    // This is Dream shops side project
+    //By Mohammed Salim
 	public static void main(String[] args) {
 		SpringApplication.run(DreamShopsApplication.class, args);
 	}
